eb4e634f91f7fb9658ef0194652d1bbd1620e00d
nvsriram <nuranisri01@gmail.com>
2024-01-26 00:02:17 -0500

    Refactor db and models

---
+++ db.py
@@ -0,0 +1,56 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import DeclarativeBase
+from json import dumps
+from config import app
+
+class Base(DeclarativeBase):
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    
+    def __repr__(self):
+        return dumps(self.as_dict(), default=str)
+    
+
+class BaseMixin(object):
+    @classmethod
+    def create(cls, **kw):
+        obj = cls(**kw)
+        db.session.add(obj)
+        db.session.commit()
+        return obj.id
+
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+with app.app_context():
+    db.reflect()
+
+
+class Player(BaseMixin, db.Model):
+    __table__ = db.metadata.tables["Player"]
+
+    @classmethod
+    def exists(self, username: str) -> int | None:
+        player = db.session.scalars(
+            db.select(self).filter_by(username=username)
+        ).first()
+        return player.id if player else None
+
+class SessionStats(BaseMixin, db.Model):
+    __table__ = db.metadata.tables["SessionStats"]
+
+
+class Session(BaseMixin, db.Model):
+    __table__ = db.metadata.tables["Session"]
+
+    @classmethod
+    def get_player_stats(self, player_id: int):
+        player_sessions = db.select(Session).filter_by(player_id=player_id).subquery()
+        session_stats = db.session.execute(
+            db.select(player_sessions, SessionStats)
+            .join(SessionStats)
+            .order_by(-SessionStats.session_end)
+        ).all()
+        return list(map(lambda stats: stats[2], session_stats))
