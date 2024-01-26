303347a554ffb7bf45aecfb05b38de4a53602545
nvsriram <nuranisri01@gmail.com>
2024-01-26 18:06:27 -0500

    Add file upload and data test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import requests
+import json
 import timeit
 from datetime import datetime, timedelta
-import json
+
+import requests
 
 # url = 'http://10.10.131.14:5000/api/predict'
 
@@ -28,13 +29,13 @@ import json
 
 # GET session
 url = "http://10.6.4.220:5000/api/session"
-data = {"username": "testuser@test.com"}
-r = requests.get(url, json=data)
-print(r.json())
+# data = {"username": "testuser@test.com"}
+# r = requests.get(url, json=data)
+# print(r.json())
 
-data = {"username": "example_username"}
-r = requests.get(url, json=data)
-print(r.json())
+# data = {"username": "example_username"}
+# r = requests.get(url, json=data)
+# print(r.json())
 
 # POST session
 session_stats = {
@@ -53,12 +54,17 @@ session_stats = {
     "s": 5,
 }
 data = {"username": "example_username", "session_stats": json.dumps(session_stats)}
-r = requests.post(url, json=data)
+inital = open("runs/detect/predict/image0.jpg", "rb")
+files = {
+    "initial_image": inital,
+    # "final_image": open("runs/detect/predict2/image0.jpg", "rb"),
+}
+r = requests.post(url, data=data, files=files)
 print(r.json())
 
-data = {"username": "testuser@test.com", "session_stats": json.dumps(session_stats)}
-r = requests.post(url, json=data)
-print(r.json())
+# data = {"username": "testuser@test.com", "session_stats": json.dumps(session_stats)}
+# r = requests.post(url, json=data)
+# print(r.json())
 
 # end = timeit.default_timer()
 # print("Request time: ", end - start)
