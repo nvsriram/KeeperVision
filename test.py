bee7dae587a3172c0802147d34c0cddbe879259d
nvsriram <nuranisri01@gmail.com>
2024-01-26 00:41:06 -0500

    Add test file

---
+++ test.py
@@ -0,0 +1,64 @@
+import requests
+import timeit
+from datetime import datetime, timedelta
+import json
+
+# url = 'http://10.10.131.14:5000/api/predict'
+
+# start = timeit.default_timer()
+
+# # GET register
+# url = "http://10.6.4.220:5000/api/register"
+# data = {"username": "testuser@test.com"}
+# r = requests.get(url, json=data)
+# print(r.json())
+
+# data = {"usernamed": "example_username"}
+# r = requests.get(url, json=data)
+# print(r.json())
+
+# # POST register
+# data = {"username": "example_username", "email": "example@email.com"}
+# r = requests.post(url, json=data)
+# print(r.json())
+
+# data = {"username": "testuser2@test.com", "email": "testuser2@test.com"}
+# r = requests.post(url, json=data)
+# print(r.json())
+
+# GET session
+url = "http://10.6.4.220:5000/api/session"
+data = {"username": "testuser@test.com"}
+r = requests.get(url, json=data)
+print(r.json())
+
+data = {"username": "example_username"}
+r = requests.get(url, json=data)
+print(r.json())
+
+# POST session
+session_stats = {
+    "session_start": (datetime.now() - timedelta(minutes=75)).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    ),
+    "session_end": (datetime.now()).strftime("%Y-%m-%d %H:%M:%S"),
+    "f": 0,
+    "b": 1,
+    "l": 2,
+    "r": 1,
+    "fl": 0,
+    "fr": 1,
+    "bl": 2,
+    "br": 3,
+    "s": 5,
+}
+data = {"username": "example_username", "session_stats": json.dumps(session_stats)}
+r = requests.post(url, json=data)
+print(r.json())
+
+data = {"username": "testuser@test.com", "session_stats": json.dumps(session_stats)}
+r = requests.post(url, json=data)
+print(r.json())
+
+# end = timeit.default_timer()
+# print("Request time: ", end - start)
